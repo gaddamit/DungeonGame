9042a418ed38671f3ecfe1bfeb26a3d2440b3e63
Leo Esguerra <leo.esguerra@gmail.com>
2023-12-27 18:28:37 -0700

    Implement view unblocker

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class FollowCamera : MonoBehaviour
 {
@@ -12,16 +13,18 @@ public class FollowCamera : MonoBehaviour
     
     [SerializeField]
     private Vector3 _offset;
-    [SerializeField]
-    private Material _transparentMat;
+    [SerializeField] private Material _originalMat;
+    [SerializeField] private Material _transparentMat;
     [SerializeField]
     private LayerMask _objectsToHideOnBlock;
     private Camera _main;
 
+    private List<GameObject> _viewBlockers;
     private float _targetDistance;
     // Start is called before the first frame update
     void Start()
     {
+        _viewBlockers = new List<GameObject>();
         _main = GetComponent<Camera>();
         LateUpdate();
         _targetDistance = Vector3.Distance(_main.transform.position, _target.transform.position);
@@ -49,8 +52,11 @@ public class FollowCamera : MonoBehaviour
         RaycastHit hitInfo;
         if(Physics.Raycast(_main.transform.position, _main.transform.TransformDirection(Vector3.forward), out hitInfo, _targetDistance, _objectsToHideOnBlock))
         {
-            Debug.Log(hitInfo.transform.gameObject.name);
             UnblockView(hitInfo.transform.gameObject);
+        }
+        else
+        {
+            ResetViewBlockers(null);
         }
 
         return false;
@@ -59,5 +65,24 @@ public class FollowCamera : MonoBehaviour
     private void UnblockView(GameObject gameObject)
     {
         gameObject.GetComponent<MeshRenderer>().material = _transparentMat;
+
+        if(!_viewBlockers.Contains(gameObject))
+        {
+            _viewBlockers.Insert(0, gameObject);
+        }
+
+        ResetViewBlockers(gameObject);
+    }
+
+    private void ResetViewBlockers(GameObject exceptGameObject)
+    {
+        for(int i = _viewBlockers.Count - 1; i >= 0; i--)
+        {
+            if(_viewBlockers[i] != exceptGameObject)
+            {
+                _viewBlockers[i].GetComponent<MeshRenderer>().material = _originalMat;
+                _viewBlockers.Remove(gameObject);
+            }
+        }
     }
 }
